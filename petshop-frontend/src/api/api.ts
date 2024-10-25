2788da0361eda0fe8d2ecea89617b324a078c626
PHCordeiro <phseabracordeiro@gmail.com>
2024-10-25 11:26:39 -0300

    FUNCIONA PF

--- petshop-frontend/src/api/api.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'http://localhost:3000', // Ajuste conforme o backend
-});
-
-export default api;
--- petshop-frontend/src/pages/Home.tsx
+++
@@ -1,9 +0,0 @@
-const Home = () => (
-    <div style={{ textAlign: 'center' }}>
-      <h1>Bem-vindo ao Pet Shop!</h1>
-      <p>Aqui você encontra os melhores serviços e cuidados para o seu pet.</p>
-    </div>
-  );
-  
-  export default Home;
-  
